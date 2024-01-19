113853058d3c4b10b97102cb23259bda53317afb
jayash-gupta <er.jayashgupta@gmail.com>
2024-01-19 22:02:25 +0530

    gp

--- backend/index.js
+++ backend/index.js
@@ -31,8 +31,7 @@ app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 app.use(
   cors({
-    origin:
-      "https://blog-app-server-2ifct7vd5-jayash-guptas-projects.vercel.app",
+    origin: "*",
     credentials: true,
   })
 );
