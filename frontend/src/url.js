5be29376b0b5ff497ae66ec4610a5b551816a1c3
jayash-gupta <er.jayashgupta@gmail.com>
2024-01-19 23:34:08 +0530

    gp

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,2 +1,2 @@
-export const URL = "https://blog-app-server-h7r5.onrender.com/";
-export const IF = "https://blog-app-server-h7r5.onrender.com/images/";
+export const URL = "https://blog-app-server-h7r5.onrender.com";
+export const IF = "https://blog-app-server-h7r5.onrender.com/images";
